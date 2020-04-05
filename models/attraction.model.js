6985b49fb01f93c2e1def8cfeb60038dfa023af6
Chinchuthakun Worameth <51841099+sincostanx@users.noreply.github.com>
2020-04-05 20:34:21 +0900

    Update attraction.model.js
    add Thai names

--- models/attraction.model.js
+++ models/attraction.model.js
@@ -2,10 +2,11 @@ const sql = require("./db.js");
 
 const Attraction = function(attraction) {
   this.attraction_name = attraction.attraction_name;
+  this.attraction_name_thai = attraction.attraction_name_thai;
   this.attraction_type = attraction.attraction_type;
   this.open_time = attraction.open_time;
   this.close_time = attraction.close_time;
-  this.attraction_description = attraction.attraction_description;
+  this.description = attraction.description;
   this.city_id = attraction.city_id;
 };
 
@@ -91,17 +92,9 @@ Attraction.getAll = result => {
 
 Attraction.updateById = (id, attraction, result) => {
   sql.query(
-    "UPDATE attraction SET attraction_name = ?, attraction_type = ?, open_time = ?, close_time = ?, attraction_description = ?, city_id = ? WHERE attraction_id = ?",
-    [
-      attraction.attraction_name,
-      attraction.attraction_type,
-      attraction.open_time,
-      attraction.close_time,
-      attraction.attraction_description,
-      attraction.city_id,
-      id
-    ],
-    (err, res) => {
+    "UPDATE attraction SET attraction_name = ?, attraction_name_thai = ?, attraction_type = ?, open_time = ?, close_time = ?, description = ?, city_id = ? WHERE attraction_id = ?",
+    [attraction.attraction_name, attraction.attraction_name_thai, attraction.attraction_type, attraction.open_time, attraction.close_time, attraction.description, attraction.city_id, id],
+	(err, res) => {
       if (err) {
         console.log("error: ", err);
         result(null, err);
