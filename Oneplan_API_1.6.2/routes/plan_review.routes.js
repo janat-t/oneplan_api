a9dc363ec9dcd30627c2bf19ce87a0558950da8f
Chinchuthakun Worameth <51841099+sincostanx@users.noreply.github.com>
2020-07-11 22:26:15 +0900

    Update plan_review.routes.js

--- Oneplan_API_1.6.2/routes/plan_review.routes.js
+++ Oneplan_API_1.6.2/routes/plan_review.routes.js
@@ -3,8 +3,8 @@ module.exports = app => {
   app.post("/api/plan_review", plan_review.create);
   app.get("/api/plan_review", plan_review.findAll);
   app.get("/api/plan_review/:reviewId", plan_review.findId);
-  app.get("/api/plan_review/plan_id/:planId", plan_review.findPlanId);
+  app.get("/api/plan_review/:planId", plan_review.findPlanId);
   app.put("/api/plan_review/:reviewId", plan_review.update);
   app.delete("/api/plan_review/:reviewId", plan_review.delete);
-  app.get("/api/plan_review", plan_review.sum);
+  app.get("/api/plan_review/:planId", plan_review.sum);
 };
