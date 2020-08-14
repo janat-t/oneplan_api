a942cd52d498cc792ab1e70b9f0e6a432fa0a0ef
Janat Taerakul <janat.taerakul@gmail.com>
2020-08-14 23:51:42 +0900

    update plan_detail.controller.js create

--- Oneplan_API_1.6.2/controllers/plan_detail.controller.js
+++ Oneplan_API_1.6.2/controllers/plan_detail.controller.js
@@ -3,27 +3,53 @@ const Attraction = require("../models/attraction.model.js");
 exports.create = (req, res) => {
   if (!req.body) {
     res.status(400).send({
-      message: "Content can not be empty."
+      message: "Content can not be empty.",
     });
   }
+  Attraction.findByGoogleId(req.body.google_place_id, (err, dummy) => {
+    if (err) {
+      if (err.kind === "not_found") {
+        const attraction = new Attraction({
+          google_place_id: req.body.google_place_id,
+        });
+        Attraction.create(attraction, (err, dummy2) => {
+          if (err)
+            res.status(500).send({
+              message:
+                err.message ||
+                "Some error occurred while creating the attraction.",
+            });
+          else console.log("updated");
+        });
+      } else {
+        res.status(500).send({
+          message:
+            "Error retrieving the attraction with google_place_id " +
+            req.params.attractionId,
+        });
+      }
+    }
 
-  const plan_detail = new Plan_detail({
-    plan_id: req.body.plan_id,
-    day: req.body.day,
-    attraction_order: req.body.attraction_order,
-    start_time: req.body.start_time,
-    end_time: req.body.end_time,
-    time_spend: req.body.time_spend,
-    google_place_id: req.body.google_place_id,
-    description: req.body.description
-  });
+    const plan_detail = new Plan_detail({
+      plan_id: req.body.plan_id,
+      day: req.body.day,
+      attraction_order: req.body.attraction_order,
+      start_time: req.body.start_time,
+      end_time: req.body.end_time,
+      time_spend: req.body.time_spend,
+      google_place_id: req.body.google_place_id,
+      description: req.body.description,
+    });
 
-  Plan_detail.create(plan_detail, (err, data) => {
-    if (err)
-      res.status(500).send({
-        message: err.message || "Some error occurred while creating the plan_detail."
-      });
-    else res.send(data);
+    Plan_detail.create(plan_detail, (err, data) => {
+      if (err)
+        res.status(500).send({
+          message:
+            err.message ||
+            "Some error occurred while creating the plan_detail.",
+        });
+      else res.send(data);
+    });
   });
 };
 
@@ -32,11 +58,12 @@ exports.duplicate = (req, res) => {
     if (err) {
       if (err.kind === "not_found") {
         res.status(404).send({
-          message: `Not found plan_detail with plan_id ${req.params.planId}.`
+          message: `Not found plan_detail with plan_id ${req.params.planId}.`,
         });
       } else {
         res.status(500).send({
-          message: "Error retrieving plan_detail with plan_id " + req.params.planId
+          message:
+            "Error retrieving plan_detail with plan_id " + req.params.planId,
         });
       }
     } else if (data.length === 0) {
@@ -52,7 +79,7 @@ exports.duplicate = (req, res) => {
           end_time: data[i].end_time,
           time_spend: data[i].time_spend,
           google_place_id: data[i].google_place_id,
-          description: data[i].description
+          description: data[i].description,
         });
         Plan_detail.create(plan_detail, (err, data2) => {
           if (err)
@@ -60,7 +87,7 @@ exports.duplicate = (req, res) => {
               message:
                 err.message ||
                 "Some error occurred while duplicating plan_detail with plan_id " +
-                  req.params.planId
+                  req.params.planId,
             });
           else array.concat(data2);
         });
@@ -74,72 +101,86 @@ exports.deletePlanIdAll = (req, res) => {
   Plan_detail.removePlanIdAll(req.params.planId, (err, data) => {
     if (err) {
       res.status(500).send({
-        message: "Could not delete plan_detail with plan_id " + req.params.planId
+        message:
+          "Could not delete plan_detail with plan_id " + req.params.planId,
       });
-    } else res.send({ message: `Plan_detail with plan_id was deleted successfully!` });
+    } else
+      res.send({
+        message: `Plan_detail with plan_id was deleted successfully!`,
+      });
   });
 };
 
 exports.deletePlanIdOne = (req, res) => {
-  Plan_detail.removePlanIdOne(req.params.planId, req.params.order, (err, data) => {
-    if (err) {
-      if (err.kind === "not_found") {
-        res.status(404).send({
-          message: `Not found plan_detail with plan_id ${req.params.planId} attraction_order ${req.params.order}..`
+  Plan_detail.removePlanIdOne(
+    req.params.planId,
+    req.params.order,
+    (err, data) => {
+      if (err) {
+        if (err.kind === "not_found") {
+          res.status(404).send({
+            message: `Not found plan_detail with plan_id ${req.params.planId} attraction_order ${req.params.order}..`,
+          });
+        } else {
+          res.status(500).send({
+            message: `Not found plan_detail with plan_id ${req.params.planId} attraction_order ${req.params.order}.`,
+          });
+        }
+      } else
+        res.send({
+          message: `Plan_detail with plan_id ${req.params.planId} attraction_order ${req.params.order}. was deleted successfully!`,
         });
-      } else {
-        res.status(500).send({
-          message: `Not found plan_detail with plan_id ${req.params.planId} attraction_order ${req.params.order}.`
-        });
-      }
-    } else
-      res.send({
-        message: `Plan_detail with plan_id ${req.params.planId} attraction_order ${req.params.order}. was deleted successfully!`
-      });
-  });
+    }
+  );
 };
 
 exports.updateOne = (req, res) => {
   if (!req.body) {
     res.status(400).send({
-      message: "Content can not be empty."
+      message: "Content can not be empty.",
     });
   }
   Attraction.findByGoogleId(req.body.google_place_id, (err, dummy) => {
     if (err) {
       if (err.kind === "not_found") {
         const attraction = new Attraction({
-			google_place_id: req.body.google_place_id
-		});
-		Attraction.create(attraction, (err, dummy2) => {
-			if (err)
-			res.status(500).send({
-				message: err.message || "Some error occurred while creating the attraction."
-			});
-		});
+          google_place_id: req.body.google_place_id,
+        });
+        Attraction.create(attraction, (err, dummy2) => {
+          if (err)
+            res.status(500).send({
+              message:
+                err.message ||
+                "Some error occurred while creating the attraction.",
+            });
+          else console.log("updated");
+        });
       } else {
         res.status(500).send({
-          message: "Error retrieving the attraction with google_place_id " + req.params.attractionId
+          message:
+            "Error retrieving the attraction with google_place_id " +
+            req.params.attractionId,
         });
       }
     }
-	Plan_detail.updateByIdOne(
-		req.params.planId,
-		req.params.order,
-		new Plan_detail(req.body),
-		(err, data) => {
-		if (err) {
-			if (err.kind === "not_found") {
-			res.status(404).send({
-				message: `Not found plan_detail with plan_id  ${req.params.planId}.`
-			});
-			} else {
-			res.status(500).send({
-				message: "Error updating plan_detail with plan_id  " + req.params.planId
-			});
-			}
-		} else res.send(data);
-		}
-	);
-  }); 
+    Plan_detail.updateByIdOne(
+      req.params.planId,
+      req.params.order,
+      new Plan_detail(req.body),
+      (err, data) => {
+        if (err) {
+          if (err.kind === "not_found") {
+            res.status(404).send({
+              message: `Not found plan_detail with plan_id  ${req.params.planId}.`,
+            });
+          } else {
+            res.status(500).send({
+              message:
+                "Error updating plan_detail with plan_id  " + req.params.planId,
+            });
+          }
+        } else res.send(data);
+      }
+    );
+  });
 };
