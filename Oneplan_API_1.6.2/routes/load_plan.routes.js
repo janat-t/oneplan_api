f0879193fa6e07e50248fb62223992978174653c
Chinchuthakun Worameth <hw.55.m.woramet@gmail.com>
2020-08-15 15:36:14 +0900

    separate fullLoad into 6 functions

--- Oneplan_API_1.6.2/routes/load_plan.routes.js
+++ Oneplan_API_1.6.2/routes/load_plan.routes.js
@@ -13,7 +13,22 @@ module.exports = app => {
 	app.get("/api/load_plan/simple", load_plan.loadSimpleId, function(req, res) {
 	 var planId = req.query.planId;
     });
-	app.get("/api/load_plan/full", load_plan.loadFullId, function(req, res) {
+	app.get("/api/load_plan/full/overview", load_plan.loadFullOverview, function(req, res) {
+	 var planId = req.query.planId;
+    });
+	app.get("/api/load_plan/full/location", load_plan.loadFullLocation, function(req, res) {
+	 var planId = req.query.planId;
+    });
+	app.get("/api/load_plan/full/tag", load_plan.loadFullTag, function(req, res) {
+	 var planId = req.query.planId;
+    });
+	app.get("/api/load_plan/full/startday", load_plan.loadFullStartday, function(req, res) {
+	 var planId = req.query.planId;
+    });
+	app.get("/api/load_plan/full/attraction", load_plan.loadFullAttraction, function(req, res) {
+	 var planId = req.query.planId;
+    });
+	app.get("/api/load_plan/full/review", load_plan.loadFullReview, function(req, res) {
 	 var planId = req.query.planId;
     });
 };
