387c2740be6cf7bee959a833cb4630e3a2bacebb
Chinchuthakun Worameth <51841099+sincostanx@users.noreply.github.com>
2020-08-15 20:22:13 +0900

    Update server.js

--- Oneplan_API_1.6.2/server.js
+++ Oneplan_API_1.6.2/server.js
@@ -30,6 +30,7 @@ require("./routes/response.routes.js")(app);
 require("./routes/plan_tag.routes.js")(app);
 require("./routes/plan_review.routes.js")(app);
 require("./routes/plan_cover.routes.js")(app);
+require("./routes/attraction_recommended.routes.js")(app);
 
 // set port, listen for requests
 
